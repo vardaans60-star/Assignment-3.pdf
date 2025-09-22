d3f45d0be6dfbe33c212e541437a682dd16c41f9
vardaans60-star <vardaans60@gmail.com>
2025-09-23 02:39:32 +0530

    Create Q5(b).c

---
+++ Q5(b).c
@@ -0,0 +1,16 @@
+/*
+  Name=Vardaan Sharma
+  Group=1A32
+  Date=7-9-2025
+  Roll No.=1025040028
+*/
+ #include <stdio.h>
+int main() {
+	int a=7;
+	printf("Enter your number:");
+	scanf("%d",&a);
+	(a%2==0)?printf("The number %d is even\n"):
+	printf("The number %d is odd");
+	(a>0)?printf("The number is %d positive"):
+	printf("The number is %d negative");	
+}
